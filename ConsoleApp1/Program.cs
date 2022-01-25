48b306ee5dde61f3d4a261dbbaa9eb11ef0c93fc
vishnukumar.ps@outlook.com <vishnukumar.ps@outlook.com>
2022-01-25 09:07:32 +0530

    functions

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Test o=new Test();
+            //o.hello();
+
+            //int number= o.getMeaRandomNum();
+            // Console.WriteLine(number);
+
+
+            // o.Add(12,12);
+
+            int avg=o.Avg(12, 12);
+            Console.WriteLine(avg);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ ConsoleApp1/Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Test
+    {
+      // with  out return type without pram
+      public void hello()
+        {
+            Console.WriteLine("hello");
+        }
+
+
+        // with return type with out parm
+        public int getMeaRandomNum()
+        {
+            Random o = new Random();
+            int OTP=o.Next(1000,2000);
+            return OTP;
+        }
+
+        // with out return type with param
+
+        public void Add( int x, int y) 
+        {
+            Console.WriteLine(x+y);
+        }
+
+        // with out return type with param
+
+        public int Avg(int x, int y)
+        {
+            return (x+y)/2;
+        }
+
+
+
+
+    }
+}
