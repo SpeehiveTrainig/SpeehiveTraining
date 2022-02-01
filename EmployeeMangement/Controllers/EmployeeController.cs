48abe0f9b0ccaae391e98532286b6ce415f833f5
vishnukumar.ps@outlook.com <vishnukumar.ps@outlook.com>
2022-02-01 22:14:24 +0530

    just commit

---
+++ EmployeeMangement/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeMangement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Hello()
+        {
+            return "hello  hai  Vishnu";
+        }
+
+
+
+        public ActionResult page2()
+        {
+            return View();
+        }
+
+
+
+
+       
+    }
+}
